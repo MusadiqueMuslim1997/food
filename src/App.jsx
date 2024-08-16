95f4adde2dd52bd640da4786624a5b2539c6cebe
MusadiqueMuslim1997 <musadique1997@gmail.com>
2024-08-16 22:57:11 +0500

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import RecipeList from './pages/RecipeList';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <NavBar />
-      {/* <RecipeCards/> */}
       <RecipeList />
+      <Footer/>
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Style.css'
+
+export default function Footer() {
+  return (
+   <footer>Copyright © 24-25 MUSADIQUE MUSLIM | All Rights Reserved</footer>
+  )
+}
