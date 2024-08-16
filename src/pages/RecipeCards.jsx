c640bba61b8740a33a90c9b7383387b96ba57f00
MusadiqueMuslim1997 <musadique1997@gmail.com>
2024-08-16 22:46:44 +0500

    add

--- src/pages/RecipeCards.jsx
+++ src/pages/RecipeCards.jsx
@@ -6,13 +6,13 @@ const RecipeCards = ({ recipe }) => {
     <div className="card">
       <div className="card-img">
         <img 
-          src={recipe.imageUrl || 'https://via.placeholder.com/150'} 
+          src={recipe.image || 'https://via.placeholder.com/150'} 
           alt={recipe.name} 
           className="recipe-card-img" 
         />
       </div>
       <div className="card-head">{recipe.name}</div>
-      <div className="card-des">{recipe.description || 'No description available.'}</div>
+      <div className="card-des">{recipe.difficulty || 'No description available.'}</div>
       <div className="card-post">Prep Time: {recipe.prepTimeMinutes} minutes</div>
     </div>
   );
